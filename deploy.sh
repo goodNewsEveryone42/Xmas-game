5d351e1bc08579235fb4563d0371dd401f6b8c86
Anna Arkhipova <qywy1234@gmail.com>
2020-12-29 23:16:02 +0300

    added github deploy for vue

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 ## if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/goodNewsEveryone42/simon-game.git master:gh-pages
+git push -f https://github.com/goodNewsEveryone42/Xmas-game.git master:gh-pages
 
 ## if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f https://<USERNAME>.github.io/<REPO> master:gh-pages
